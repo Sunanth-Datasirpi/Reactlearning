73c8e99af2a875d4374e590ecefc88dabf9bfbf7
Sunanth-Datasirpi <sunanth.h@datasirpi.com>
2023-10-20 15:13:54 +0530

    adding dark theme

--- router/src/App.js
+++ router/src/App.js
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import React from "react";
+import React, { createContext, useState, useContext } from "react";
 import "./App.css";
 import Home from "./components/Home";
 import { Navbar } from "./components/Navbar";
@@ -18,36 +18,48 @@ import Login from "./components/Login";
 import { AuthProvider } from "./components/Auth";
 const LazyAbout = React.lazy(() => import("./components/About"));
 
+export const themeContext = createContext(null);
+
 function App() {
+  const [theme, setTheme] = useState("dark");
+
+  const toggleTheme = () => {
+    setTheme((curr) => (curr === "light" ? "dark" : "light"));
+  };
+
   return (
     <AuthProvider>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route
-          path="about"
-          element={
-            <React.Suspense fallback="Loading....">
-              <LazyAbout />
-            </React.Suspense>
-          }
-        />
-        <Route path="order-summary" element={<OrderSummary />} />
-        <Route path="products" element={<Products />}>
-          <Route index element={<FeaturedProducts />} />
-          <Route path="featured" element={<FeaturedProducts />} />
-          <Route path="new" element={<NewProducts />} />
-        </Route>
-        <Route path="users" element={<Users />}>
-          <Route path=":Id" element={<UserDetails />} />
-          <Route path="admin" element={<Admin />} />
-        </Route>
-        <Route path="profile" element={<Profile />} />
-        <Route path="login" element={<Login />} />
-        <Route path="*" element={<NoMatch />} />
-      </Routes>
-      <PortalDemo />
-      <PortalsWorking />
+      <themeContext.Provider value={{ theme, toggleTheme }}>
+        <div id={theme}>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route
+              path="about"
+              element={
+                <React.Suspense fallback="Loading....">
+                  <LazyAbout />
+                </React.Suspense>
+              }
+            />
+            <Route path="order-summary" element={<OrderSummary />} />
+            <Route path="products" element={<Products />}>
+              <Route index element={<FeaturedProducts />} />
+              <Route path="featured" element={<FeaturedProducts />} />
+              <Route path="new" element={<NewProducts />} />
+            </Route>
+            <Route path="users" element={<Users />}>
+              <Route path=":Id" element={<UserDetails />} />
+              <Route path="admin" element={<Admin />} />
+            </Route>
+            <Route path="profile" element={<Profile />} />
+            <Route path="login" element={<Login />} />
+            <Route path="*" element={<NoMatch />} />
+          </Routes>
+          <PortalDemo />
+          <PortalsWorking />
+        </div>
+      </themeContext.Provider>
     </AuthProvider>
   );
 }
